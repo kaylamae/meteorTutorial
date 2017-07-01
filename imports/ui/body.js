bd82b039b8813467f8fbebc4919a1ef805b612d1
dizonwork <dizonwork@gmail.com>
2017-07-01 11:59:53 +0800

    first commit

---
+++ imports/ui/body.js
@@ -0,0 +1,27 @@
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.js'
+import './body.html';
+
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
+
+Template.body.events({
+  'submit .new-task' (event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+    target.text.value = '';
+  },
+});
